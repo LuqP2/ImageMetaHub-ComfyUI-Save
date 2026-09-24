0a52b5ecd9073249b27f491162919c08bed3f2b6
LuqP2 <kelipot.l@gmail.com>
2026-09-24 11:11:56 -0300

    chore: bump Save Node to 1.2.2

--- metadata_utils_impl.py
+++ metadata_utils_impl.py
@@ -14,7 +14,7 @@ from xml.sax.saxutils import escape as xml_escape
 import numpy as np
 from PIL import Image, PngImagePlugin
 
-METAHUB_SAVE_NODE_VERSION = "1.2.1"
+METAHUB_SAVE_NODE_VERSION = "1.2.2"
 
 try:
     import piexif
